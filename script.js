92f738c89e160572982718c013b00e5730c2bd86
Luiz Henrique <luiz0915henrique@gmail.com>
2026-03-08 17:38:44 -0300

    Atualiza site: cardápio e CSS/JS

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-const toggle = document.querySelector(".menu-toggle")
-const nav = document.querySelector("nav")
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
 
-toggle.addEventListener("click", () => {
-nav.classList.toggle("active")
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+const botoesFiltro = document.querySelectorAll(".filtro button");
+const produtos = document.querySelectorAll(".produto");
+
+botoesFiltro.forEach(botao => {
+    botao.addEventListener("click", () => {
+        botoesFiltro.forEach(b => b.classList.remove("ativo"));
+        botao.classList.add("ativo");
+
+        const categoria = botao.getAttribute("data-filtro");
+        produtos.forEach(produto => {
+            if (categoria === "todos" || produto.dataset.categoria === categoria) {
+                produto.computedStyleMap.display = "block";
+            }
+             else {
+                produto.style.display = "none";
+            
+        }
+    });
+        
+    })
 })
